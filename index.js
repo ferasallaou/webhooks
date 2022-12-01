5d98d9d709f1aff6f0ad12770f65c81cafa659f3
Feras Allaou <feras@Ferass-MacBook-Pro.local>
2022-12-01 13:03:26 +0300

    added ody parser

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     console.log(req.body)
